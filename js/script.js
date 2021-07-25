623183ebf03d9815ac97d2fb63f43245aebcaa9e
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-07-26 01:56:53 +0300

    re-did the js section of what we do

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ $(document) .ready(function(){
         console.log(scroll); 
     })
 })
+
+
+// what to do section
+
+$(document).ready(function(){
+    $(".item-1").click(function(){
+        $(".figcaption1").toggle();
+        $(".img1").toggle();
+    })
+    $(".item-2").click(function(){
+        $(".hide").toggle();
+        $(".show").toggle();
+    })
+
+    $(".item-3").click(function(){
+        $(".hide").toggle();
+        $(".show").toggle();
+    })
+
+
+});
